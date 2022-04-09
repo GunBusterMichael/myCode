c38bdb572a6f742cb9bbf3f7716c2fee3b039bcf
GunBusterMichael <hwer325@163.com>
2022-04-10 00:58:30 +0800

    添加 2022年4月9日 学习文件

--- Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/011-todo案例/js/index.js
+++ Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/011-todo案例/js/index.js
@@ -7,21 +7,56 @@ const app = new Vue({
     checkMark: '',
     isChangeContent: '',
     isShowDel: '',
-    isAllCompleted: ''
+    isAllCompleted: false,
+    activeNum: 0
   },
   filters: {
-
+    showCheckMark(value) {
+      let checkMark = ''
+      checkMark = value === true ? '√' : ''
+      return checkMark
+    }
+  },
+  computed: {
+    activeStatus() {
+      this.activeNum = 0
+      // 因为 forEach 内部的 this 不再指向 Vue 实例，所以需要提前保存 Vue 实例，或者用其他方法
+      for (let item of this.todos) {
+        if (item.isCompleted === false) {
+          this.activeNum++
+        }
+      }
+      if (this.activeNum > 0) {
+        return this.activeNum + '项未完成'
+      } else {
+        return '都完成了'
+      }
+    },
+    isShowDelCompletedTodos() {
+      if (this.activeNum < this.todos.length) {
+        return true
+      } else {
+        return false
+      }
+    },
+    isShowTodoList() {
+      if (this.todos.length === 0) {
+        return false
+      } else {
+        return true
+      }
+    }
   },
   methods: {
     createTodo() {
-      if (this.inputContent.content === '') return;
+      if (this.inputContent === '') return;
       this.todos.unshift({
         content: this.inputContent,
         isCompleted: false,
         checkMark: '',
-        isDeeper: false,
         isChangeContent: false,
-        isShowDel: false
+        isShowDel: false,
+        isShowTodo: true
       })
       this.inputContent = ''
       this.isItalic = true
@@ -35,8 +70,14 @@ const app = new Vue({
     },
     switchCompleted(item) {
       item.isCompleted = !item.isCompleted
-      item.checkMark = item.isCompleted === true ? '√' : ''
-      item.isDeeper = !item.isDeeper
+      let flag = true
+      for (item of this.todos) {
+        if (item.isCompleted === false) {
+          flag = false
+          break
+        }
+      }
+      this.isAllCompleted = flag
     },
     startChangeContent(item) {
       item.isChangeContent = true
@@ -52,16 +93,46 @@ const app = new Vue({
     },
     switchShowDel(item) {
       item.isShowDel = !item.isShowDel
+      // console.log('switchShowDel');
     },
     delTodo(index) {
+      if (index + 1 != this.todos.length) {
+        this.todos[index + 1].isShowDel = true
+      }
       this.todos.splice(index, 1)
     },
     switchAllCompleted() {
-      for (let item of todos) {
-        
-        item.isCompleted = true
+      if (this.todos.length === 0) return;
+      if (this.isAllCompleted === false) {
+        for (let item of this.todos) {
+          item.isCompleted = !this.isAllCompleted
+        }
+        this.isAllCompleted = !this.isAllCompleted
+      } else {
+        for (let item of this.todos) {
+          item.isCompleted = !this.isAllCompleted
+        }
+        this.isAllCompleted = !this.isAllCompleted
       }
-      this.checkMark = '√'
+    },
+    showAllTodos() {
+      this.todos.forEach(function (item) {
+        item.isShowTodo = true
+      })
+    },
+    showActiveTodos() {
+      this.todos.forEach(function (item) {
+        item.isShowTodo = !item.isCompleted
+      })
+    },
+    showCompletedTodos() {
+      this.todos.forEach(function (item) {
+        item.isShowTodo = item.isCompleted
+      })
+    },
+    delCompletedTodos() {
+      this.todos = this.todos.filter(item => item.isCompleted === false)
+      this.isAllCompleted = false
     }
   },
   // 使用生命周期函数mounted配合属性ref="input"实现autofocus
@@ -71,3 +142,18 @@ const app = new Vue({
     })
   }
 })
+
+// 禁止手指双击放大、缩小页面
+var lastTouchEnd = 0;
+document.documentElement.addEventListener('touchend', function (event) {
+  var now = Date.now();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, {
+  passive: false
+});
+
+// 特别感谢
+console.log('特别感谢：A-SOUL https://space.bilibili.com/703007996/');
