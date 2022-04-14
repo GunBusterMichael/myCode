e9b043ac4c3fa559e9315617367eb2322e707451
GunBusterMichael <hwer325@163.com>
2022-04-14 17:13:04 +0800

    第一次提交 2022年4月14日 学习文件

---
+++ Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/LearnVuejs_v2_开始引入webpack/014-webpack的使用/014-02-webpack的配置/src/main.js
@@ -0,0 +1,10 @@
+import {add, mul} from './mathUtils.js'
+
+console.log(add(20, 30))
+console.log(mul(20, 30))
+
+import {name, age, height} from './info'
+
+console.log(name)
+console.log(age)
+console.log(height)
