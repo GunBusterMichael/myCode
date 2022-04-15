f27d95c2ec4764817a80b7bb89a60a9017367806
GunBusterMichael <hwer325@163.com>
2022-04-16 01:39:37 +0800

    第四次提交 2022年4月15日 学习文件

---
+++ Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/014-webpack的使用/014-06-webpack配置分离/src/vue/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h2 class="title">{{message}}</h2>
+    <button @click="btnClick">按钮</button>
+    <h2>{{name}}</h2>
+    <Cpn/>
+  </div>
+</template>
+
+<script>
+  // 将 Vue 实例需要使用到的组件 Cpn 引入到 Vue 实例模板的 .vue 文件中。
+  import Cpn from './Cpn.vue'
+
+  export default {
+    data() {
+      return {
+        message: "Hello Vue!",
+        name: "Michael",
+      };
+    },
+    methods: {
+      btnClick() {
+        console.log("按钮被点击了");
+      },
+    },
+    components: {
+      Cpn
+    },
+  }
+</script>
+
+<style scoped>
+  .title {
+    color: green;
+  }
+</style>
