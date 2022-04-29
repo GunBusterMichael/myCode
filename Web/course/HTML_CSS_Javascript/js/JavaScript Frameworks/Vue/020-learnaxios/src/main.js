652808fe31afa467f53215af6ef8448c5e1537cd
GunBusterMichael <hwer325@163.com>
2022-04-29 12:45:55 +0800

    第一次提交 2022年4月29日 学习文件

--- Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/020-learnaxios/src/main.js
+++ Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/020-learnaxios/src/main.js
@@ -11,7 +11,21 @@ new Vue({
 
 /* 调用 axios */
 axios({
-  url: 'http://www.blogry.cn/test/index'
+  url: 'http://152.136.185.210:7878/api/hy66/home/multidata',
+  method: 'get'  /* 发送 get 请求 */
+  // method: 'post'  /* 发送 post 请求 */
 }).then(re => {    /* axios 会返回 promise 实例，re 为请求到的数据 */
+  console.log(re)
+})
+
+/* 向有参数的 url 发送请求 */
+axios({
+  url: 'http://152.136.185.210:7878/api/hy66/home/multidata',
+  // 针对 get 请求的参数拼接
+  params: {
+    type: 'pop',
+    page: 1
+  }
+}).then(re => {
   console.log(re)
 })
