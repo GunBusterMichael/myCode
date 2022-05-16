56f6a8d87e4361d5051eb069941dc2a8c992a20a
GunBusterMichael <hwer325@163.com>
2022-05-17 01:23:29 +0800

    提交 2022年5月16日 学习文件

---
+++ Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/Vue2/travel/src/main.js
@@ -0,0 +1,24 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入 fastclick 库，解决移动端点击300ms延迟问题
+import fastClick from 'fastclick'
+// 引入能让所有手机显示效果一致的 css 文件
+import 'assets/styles/reset.css'
+// 引入解决 1px border 问题的 css 文件
+import 'assets/styles/border.css'
+
+Vue.config.productionTip = false
+
+// 使用 fastClick 库
+fastClick.attach(document.body)
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ Web/course/HTML_CSS_Javascript/js/JavaScript Frameworks/Vue/Vue2/travel/src/pages/home/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="header">
+    <div class="header-left">返回</div>
+    <div class="header-input">输入城市/景点/游玩/主题</div>
+    <div class="header-right">城市</div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Header",
+    
+  }
+</script>
+
+<style lang="stylus" scoped>
+  /* 1rem = html的font-size = 50px */
+  .header
+    display: flex
+    height: .86rem
+    background: #00bcd4
+    color: #fff
+    line-height .86rem
+    .header-left
+      width: .64rem
+      float: left
+    .header-input
+      flex: 1
+      background-color #fff
+      border-radius: .1rem
+      height: .64rem
+      margin-top .11rem
+      margin-left .2rem
+      color: #ccc
+      line-height .64rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+</style>
